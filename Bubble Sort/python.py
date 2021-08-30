85721c7d2c044526e44b8c39a00a9f86b7abc04a
Kunal Attri <attrikunal16@gmail.com>
2021-08-30 15:53:44 +0530

    added bubble sort code

---
+++ Bubble Sort/python.py
@@ -0,0 +1,20 @@
+# Python algorithm for bubblesort
+
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+print("Initial array: ", arr)
+  
+bubbleSort(arr)
+
+print("Sorted array: ", arr)
+  
