0a4fa8f4e34801e57d3701d0b019cadbcc82eb5c
marooncoder09 <spratap.03.09@gmail.com>
2021-09-07 21:43:05 +0530

    Added Insertion Sort Code in Java Language

---
+++ Insertion/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.apnacollege;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,2,5,11,9,0};
+       InsertionSortAlgo(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+    static void InsertionSortAlgo(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j >0 ; j--) {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
