01831b7845826ad7cfd9f4761bdf245eaa99a2d2
Kunal Attri <35298757+Kunal-Attri@users.noreply.github.com>
2021-09-07 22:22:51 +0530

    printing array before sorting and mentions

--- Insertion/InsertionSort.java
+++ Insertion/InsertionSort.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] nums = {1,4,2,5,11,9,0};
+        System.out.print("Initial Array: ");
+        System.out.println(Arrays.toString(nums));
        InsertionSortAlgo(nums);
+        System.out.print("Sorted Array: ");
         System.out.println(Arrays.toString(nums));
 
     }
